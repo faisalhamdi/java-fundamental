865dc53e249924c8d56a26899ae8e6d051182331
jambrongbro <jambrongbro17@gmail.com>
2020-05-18 01:45:39 +0700

    object

---
+++ src/com/dicoding/javafundamental/object/Main.java
@@ -0,0 +1,11 @@
+package com.dicoding.javafundamental.object;
+
+public class Main {
+    public static void main(String[] x) {
+        Hewan hewan = new Hewan("Singa");
+
+        hewan.beratHewan(200);
+        hewan.jumlahKakiHewan(4);
+        hewan.cetakHewan();
+    }
+}
